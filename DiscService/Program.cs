73f7b91f74c0db7945ea07a58609f8a1214b5d9f
LMax <max.leontiev@mail.ru>
2025-05-22 17:01:00 +0500

    Initial commit

---
+++ DiscService/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DiscService;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddHostedService<CommandWorkerService>();
+            })
+            .Build();
+
+        await host.RunAsync();
+    }
+}
